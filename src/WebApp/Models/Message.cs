d40ec0cdbc8c1bac15d51be8f054bc174eaefed0
Luca <20096550@student.hhs.nl>
2022-01-07 17:15:13 +0100

    Added DateTimeSent to the Message class

--- src/WebApp/Models/Message.cs
+++ src/WebApp/Models/Message.cs
@@ -2,6 +2,7 @@ public class Message
 {
     public int Id { get; set; }
     public string Text { get; set; }
+    public string DateTimeSent { get; set; }
     public ApplicationUser Sender { get; set; }
     public Chat Chat { get; set; }
 }
