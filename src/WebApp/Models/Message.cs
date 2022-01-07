8c39024ddac623f795111bfb2bce31b64e5ca489
Luca <20096550@student.hhs.nl>
2022-01-07 17:46:54 +0100

    Fixed DateTime in Message.cs

--- src/WebApp/Models/Message.cs
+++ src/WebApp/Models/Message.cs
@@ -1,8 +1,10 @@
+using System;
+
 public class Message
 {
     public int Id { get; set; }
     public string Text { get; set; }
-    public string DateTimeSent { get; set; }
+    public DateTime DateTimeSent { get; set; }
     public ApplicationUser Sender { get; set; }
     public Chat Chat { get; set; }
 }
