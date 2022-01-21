f4bf1c71190871b302fd407bf0f9fc7ba09500c9
Derman <derman7891@live.nl>
2022-01-21 15:42:30 +0100

    fixed role

--- src/WebApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/WebApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -79,11 +79,11 @@ namespace WebApp.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 var user = new ApplicationUser { ClientId = Input.ClientId, UserName = Input.Email, Email = Input.Email, Guides = new List<ApplicationUser>(), GuidedBy = new List<ApplicationUser>(), Appointments = new List<Appointment>(), Files = new List<File>(), Groups = new List<Group>(), PrivateChats = new List<PrivateChat>() };
-                await _userManager.AddToRoleAsync(user, "Client");
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
+                    await _userManager.AddToRoleAsync(user, "Client");
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
